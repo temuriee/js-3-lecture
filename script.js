934aab3b6d683f90611530c94b08eb93ee8fd80a
temuriee <critstoggle@gmail.com>
2025-05-19 22:00:56 +0400

    structure

---
+++ script.js
@@ -0,0 +1,33 @@
+// let myName = "Temo";
+
+// let greeting = "temurie";
+// let greeting2 = `Hello`;
+
+// console.log(greeting + " " + myName);
+// let sum = `${greeting} ${myName}! Welcome Back`;
+// console.log(`${greeting} ${myName}! Welcome Back`);
+// console.log(`${greeting} World`);
+// console.log(sum.length);
+// console.log(sum[23]);
+
+// //
+// console.log(sum.toUpperCase());
+
+// console.log(sum.toLowerCase());
+// let greeting = "Mindia";
+// let greeting2 = "Hello World";
+// console.log(greeting.lastIndexOf("i"));
+// console.log(greeting2.includes("Hello"));
+
+// if (greeting2.includes("Hello")) {
+//   console.log("es winadadeba sheicavs sityva hellos");
+// } else {
+//   console.log("es winadadeba ar sheicavs sityva hellos");
+// }
+let userName = prompt("daweret mxolod tqveni saxeli");
+
+if (userName.includes(" ")) {
+  console.log("ar unda sheicavdes space-s");
+} else {
+  console.log(`Hello, ${userName}`);
+}
